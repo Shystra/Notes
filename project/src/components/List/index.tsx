87a2d8a6491d597925bd208a2cf9c9abb916157a
Shystra <jlsp.2019@hotmail.com>
2023-07-24 22:07:01 -0300

    fate: add and delete components

--- project/src/components/List/index.tsx
+++ project/src/components/List/index.tsx
@@ -1,10 +1,33 @@
 import styles from './List.module.css';
+// import {ReactComponent as Check} from '../../assets/check.svg';
+import {BsCheckCircle} from 'react-icons/bs';
+import {BiSolidEdit} from 'react-icons/bi';
+import {AiOutlineDelete} from 'react-icons/ai';
+import { ITodo } from '../../pages/Home';
 
-export function List(){
+interface IProps{
+    todos: ITodo[];
+    deleteTodo: (id:string) => void
+}
+
+export function List({todos, deleteTodo}: IProps){
+   
     return (
-        <div className={styles.card}>
-            <h1>list</h1>
-            <span>icon</span>
-        </div>
+        <>
+        {todos.map((todo) =>{
+            return (
+
+                <div className={styles.card}>
+                    <input type="text" value={todo.description}/>
+                    <div className={styles.icon}>
+                        <BsCheckCircle/>
+                        <BiSolidEdit/>
+                        <AiOutlineDelete onClick={() => deleteTodo(todo.id)}/>
+                    </div>
+                </div>
+            );
+
+             })}
+        </>
     )
 }
--- project/src/pages/Home/index.tsx
+++ project/src/pages/Home/index.tsx
@@ -1,24 +1,56 @@
 import { Header } from '../../components/Header';
 import { List } from '../../components/List';
 import styles from './Home.module.css';
+import {useState} from 'react';
+import { v4 as uuid}  from 'uuid';
 
+export interface ITodo{
+    id: string,
+    description: string,
+    completed: boolean;
+}
 
 export function Home (){
+    const [todo, setTodo] = useState<string>('');
+    const [todos, setTodos] = useState<ITodo[]>([]);
+    
+
+    
+    const addTodo = () => {
+        const newTodo = {
+            id: uuid(),
+            description: todo,
+            completed: false
+        };
+        setTodos([...todos, newTodo]);
+        setTodo('');
+        // console.log('testing', newTodo)
+    }
+    // console.log('todos',todos)
+
+    const deleteTodo = (id) => {
+        const filterTodos = todos.filter(todo => todo.id != id)
+
+        setTodos(filterTodos);
+    }
+
     return(
         <div>
             <Header/>
             <div className={styles.createTask}>
-                <input type='text'/>
-                <button>Adicionar</button>
+                <input 
+                type='text' value={todo} 
+                onChange={(e) => setTodo(e.target.value)}/>
+                <button onClick={addTodo}>Adicionar</button>
             </div>
             <div className={styles.filter}>
                 <span className={styles.finish}>Finalizados: 5 tarefas</span>
                 <span className={styles.progress}>Em progresso: 5 tarefas</span>
             </div>
 
-            <List/>
-            <List/>
-            <List/>
+            <List todos={todos} deleteTodo={deleteTodo}/>
+
+            
 
         </div>
     )
